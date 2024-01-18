e429c0506652ae3c5fe246239542db64121c7680
beomsuyoo <beomsuyoo@gmail.com>
2024-01-18 20:58:24 +0900

    first commit

---
+++ src/main/java/com/example/globalStudents/GlobalStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.globalStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/globalStudents/GlobalStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globalStudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
