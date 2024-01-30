31281f541aa4664ff40ff005cc049969b2c22d33
boms03 <beomsuyoo@gmail.com>
2024-01-30 21:16:30 +0900

    #4 [FEAT] 회원가입과 로그인에 필요한 entity 추가 완료

---
+++ src/main/java/com/example/globalStudents/domain/user/entity/CountryEntity.java
@@ -0,0 +1,26 @@
+package com.example.globalStudents.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "country")
+public class CountryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "nationality", cascade = CascadeType.ALL)
+    private List<UserEntity> CountryUserList = new ArrayList<>();
+
+}
