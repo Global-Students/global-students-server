d46694d449618efbd944d5abb22a7195074b66a6
boms03 <beomsuyoo@gmail.com>
2024-02-06 17:57:53 +0900

    #60 [FEAT] 회원가입 기능 구현 완료

--- src/main/java/com/example/globalStudents/domain/user/controller/AuthController.java
+++ src/main/java/com/example/globalStudents/domain/user/controller/AuthController.java
@@ -2,42 +2,44 @@ package com.example.globalStudents.domain.user.controller;
 
 import com.example.globalStudents.domain.user.dto.UserRequestDTO;
 import com.example.globalStudents.domain.user.dto.UserResponseDTO;
+import com.example.globalStudents.domain.user.service.MailService;
+import com.example.globalStudents.domain.user.service.MailServiceImpl;
 import com.example.globalStudents.domain.user.service.UserService;
 import com.example.globalStudents.global.apiPayload.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/auth/join")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
 
-//    @PostMapping("/join/information")
-//    public ApiResponse<UserResponseDTO.JoinResultDTO> join(
-//            @RequestBody
-//            UserRequestDTO.JoinDTO joinDTO
-//    ){
-//        UserResponseDTO.JoinResultDTO user = userService.createUser(joinDTO);
-//        return ApiResponse.onCreated(user);
-//    }
+    @PostMapping("/information")
+    public ApiResponse<UserResponseDTO.JoinResultDTO> join(
+            @RequestBody
+            UserRequestDTO.JoinDTO joinDTO
+    ){
+        UserResponseDTO.JoinResultDTO user = userService.createUser(joinDTO);
+        return ApiResponse.onCreated(user);
+    }
 
-    @GetMapping("/join/check-id/{user_id}")
-    public ApiResponse<UserResponseDTO.CheckIdDTO> checkId(
+    @GetMapping("/check-id/{user_id}")
+    public ApiResponse<UserResponseDTO.CheckIdResultDTO> checkId(
             @PathVariable
             String user_id
     ){
-        UserResponseDTO.CheckIdDTO id = userService.checkId(user_id);
+        UserResponseDTO.CheckIdResultDTO id = userService.checkId(user_id);
         return ApiResponse.onSuccess(id);
     }
 
-    @GetMapping("/join/check-nickname/{user_nickname}")
-    public ApiResponse<UserResponseDTO.CheckNicknameDTO> checkNickname(
+    @GetMapping("/check-nickname/{user_nickname}")
+    public ApiResponse<UserResponseDTO.CheckNicknameResultDTO> checkNickname(
             @PathVariable
             String user_nickname
     ){
-        UserResponseDTO.CheckNicknameDTO nickname = userService.checkNickname(user_nickname);
+        UserResponseDTO.CheckNicknameResultDTO nickname = userService.checkNickname(user_nickname);
         return ApiResponse.onCreated(nickname);
     }
 
