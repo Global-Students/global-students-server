d6005a78beda838570a9a1e1aa7df0653be5a8d4
boms03 <beomsuyoo@gmail.com>
2024-02-12 16:09:20 +0900

    #89 [FEAT] 유저 신고된 횟수 기능 추가 완료

--- src/main/java/com/example/globalStudents/domain/board/repository/ReportRepository.java
+++ src/main/java/com/example/globalStudents/domain/board/repository/ReportRepository.java
@@ -1,7 +1,9 @@
 package com.example.globalStudents.domain.board.repository;
 
 import com.example.globalStudents.domain.board.entity.ReportEntity;
+import com.example.globalStudents.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
+    int countAllByReportedUser(UserEntity reportedUser);
 }
