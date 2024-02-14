fcab958c04a4064417a56bf3bec70344286d562b
sskys18 <123645816+sskys18@users.noreply.github.com>
2024-02-14 13:05:38 +0900

    [feat] userimage upload api 추가

---
+++ src/main/java/com/example/globalStudents/domain/myPage/service/UserImageService.java
@@ -0,0 +1,9 @@
+package com.example.globalStudents.domain.myPage.service;
+
+import com.example.globalStudents.domain.myPage.dto.UserImageResponseDTO;
+import com.example.globalStudents.domain.myPage.enums.ImageType;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface UserImageService {
+    UserImageResponseDTO.UploadUserImageResultDTO uploadUserImage(MultipartFile multipartFile, ImageType imageType, Long userId);
+}
