f5361bbe4b6563902d818f56f79eadaea23dd644
yunju <juile0827@naver.com>
2024-02-18 17:35:49 +0900

    SecurityConfig.java api 인증 범위 설정

--- src/main/java/com/example/globalStudents/global/auth/SecurityConfig.java
+++ src/main/java/com/example/globalStudents/global/auth/SecurityConfig.java
@@ -96,10 +96,12 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/error", "/v3/**", "/swagger-ui/**", "/auth/**", "/user/**", "/refresh", "/health").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/", "/error", "/v3/**", "/swagger-ui/**", "/auth/**", "/user/**", "/refresh", "/health","/search/university","/boards/{board_id}/posts/{post_id}","/boards/{board_id}").permitAll()
+                        .requestMatchers("/admin/**","/search/**","/mypage/**", "/friends/**","/boards/**").hasRole("ADMIN")
+                        .requestMatchers("/search/**", "/mypage/**","/friends/**","/boards/**").hasRole("USER")
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                         .anyRequest().authenticated());
+
 
         http
                 .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
